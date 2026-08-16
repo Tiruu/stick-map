851e98e9b30d61030bd731a483e9667baacfe6c7
Tiruu <97034382+Tiruu@users.noreply.github.com>
2026-08-16 21:19:18 +0200

    reverted

--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -70,30 +70,7 @@ export function useMap({
 
     const map = new Map({
       container: mapContainer.current,
-      style: {
-            version: 8,
-            sources: {
-                // Source pour l'imagerie satellite ESRI
-                'satellite-tiles': {
-                    type: 'raster',
-                    tiles: [
-                        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-                    ],
-                    tileSize: 256,
-                    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-                }
-            },
-            layers: [
-                // Ajout de la couche de fond satellite
-                {
-                    id: 'satellite-layer',
-                    type: 'raster',
-                    source: 'satellite-tiles',
-                    minzoom: 0,
-                    maxzoom: 19
-                }
-            ]
-        },
+      style: "https://tiles.openfreemap.org/styles/liberty",
       center: [3.57, 47.8],
       zoom: 12,
       dragRotate: false,
